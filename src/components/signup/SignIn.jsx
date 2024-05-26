037012adf32ac65dd4ea8ea3e29c921e55e2e5de
Fahmida Rahman <121882323+007fahmida@users.noreply.github.com>
2024-05-26 21:14:41 +0600

    Update SignIn.jsx

--- src/components/signup/SignIn.jsx
+++ src/components/signup/SignIn.jsx
@@ -1,35 +1,68 @@
-import React from "react";
-import "./Signup.css";
+import axios from "axios";
 import HeadingComp from "./HeadingComp";
+import "./Signup.css";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authActions } from "../../store";
 
 const SignIn = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [inputs, setInputs] = useState({
+    email: "",
+    password: "",
+  });
+
+  const change = (e) => {
+    const { name, value } = e.target;
+    setInputs({ ...inputs, [name]: value });
+  };
+
+  const submit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post("https://mern-todo-backend-2-q1jw.onrender.com/api/v1/signin", inputs);
+      if (response.data.others) {
+        sessionStorage.setItem("id", response.data.others._id);
+        dispatch(authActions.login());
+        navigate("/todo");
+      } else {
+        console.error("Error:", response.data.message);
+      }
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
+
   return (
-    <div>
-      {" "}
-      <div className="signup">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4 column col-left d-flex justify-content-center align-items-center">
-              <HeadingComp first="Sign" second="In" />
-            </div>
-
-            <div className="col-lg-8 column  d-flex justify-content-center align-items-center">
-              <div className="d-flex flex-column w-100 p-5">
-                <input
-                  className="p-2 my-3 input-signup"
-                  type="email"
-                  name="email"
-                  placeholder="Enter Your Email"
-                />
-
-                <input
-                  className="p-2 my-3 input-signup"
-                  type="password"
-                  name="password"
-                  placeholder="Enter Your Password"
-                />
-                <button className="btn-signup p-2">Sign In</button>
-              </div>
+    <div className="signup">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4 column col-left d-none d-lg-flex justify-content-center align-items-center">
+            <HeadingComp first="Sign" second="In" />
+          </div>
+          <div className="col-lg-8 column d-flex justify-content-center align-items-center">
+            <div className="d-flex flex-column w-100 p-3">
+              <input
+                className="p-2 my-3 input-signup"
+                type="email"
+                name="email"
+                placeholder="Enter Your Email"
+                value={inputs.email}
+                onChange={change}
+              />
+              <input
+                className="p-2 my-3 input-signup"
+                type="password"
+                name="password"
+                placeholder="Enter Your Password"
+                value={inputs.password}
+                onChange={change}
+              />
+              <button className="btn-signup p-2" onClick={submit}>
+                Sign In
+              </button>
             </div>
           </div>
         </div>
@@ -37,4 +70,5 @@ const SignIn = () => {
     </div>
   );
 };
+
 export default SignIn;
