ceb076e5c1110b14c3f91671f9895888b8695370
Fahmida Rahman <121882323+007fahmida@users.noreply.github.com>
2024-05-26 21:17:00 +0600

    Update TodoCards.jsx

--- src/components/todo/TodoCards.jsx
+++ src/components/todo/TodoCards.jsx
@@ -1,20 +1,40 @@
 import React from "react";
+import { GrDocumentUpdate } from "react-icons/gr";
 import { MdDelete } from "react-icons/md";
-import { GrUpdate } from "react-icons/gr";
 
-const TodoCards = ({title,body}) => {
+const TodoCards = ({
+  title,
+  body,
+  id,
+  delid,
+  display,
+  updateId,
+  toBeUpdate,
+}) => {
   return (
     <div className="p-3 todo-card">
       <div>
         <h5>{title}</h5>
-        <p className="todo-cards-p">
-            {body.split("", 77)}...
-        </p>
+        <p className="todo-card-p">{body.split("", 77)}...</p>
       </div>
-      <div>
-        <div><GrUpdate /></div>
-        <div><MdDelete /></div>
-      
+      <div className="d-flex justify-content-around ">
+        <div
+          className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1 "
+          onClick={() => {
+            display("block");
+            toBeUpdate(updateId);
+          }}
+        >
+          <GrDocumentUpdate className="card-icons" /> Update
+        </div>
+        <div
+          className="d-flex justify-content-center align-items-center card-icon-head px-2 py-1 text-danger"
+          onClick={() => {
+            delid(id);
+          }}
+        >
+          <MdDelete className="card-icons del" /> Delete
+        </div>
       </div>
     </div>
   );
