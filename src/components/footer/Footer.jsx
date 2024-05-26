72278ce04b2dcc0b918a6b12c94c672d67e5e390
Fahmida Rahman <121882323+007fahmida@users.noreply.github.com>
2024-05-26 21:09:56 +0600

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,25 +1,10 @@
-// // src/components/Footer.jsx
-// import React from "react";
-// import "./Footer.css";
-
-// const Footer = () => {
-//   return (
-//     <div className="container-fluid p-3 d-flex justify-content-center align-items-center text-white footer">
-//        <p className="m-0">&copy;tOdO2024 | All rights reserved</p>
-//     </div>
-//   );
-// };
-
-// export default Footer;
-
-// src/components/Footer.jsx
 import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
   return (
     <div className="container-fluid p-3 d-flex justify-content-center align-items-center footer">
-       <p className="m-0">&copy;tOdO2024 | All rights reserved</p>
+       <p className="m-0">&copy;todo2024 | All rights reserved</p>
     </div>
   );
 };
