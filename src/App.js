46dd666b6b73d7f954753d443de384cf99d92113
Fahmida Rahman <121882323+007fahmida@users.noreply.github.com>
2024-05-26 21:22:45 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./components/about/About";
 import Footer from "./components/footer/Footer";
 import Home from "./components/home/Home";
@@ -7,18 +5,32 @@ import Navbar from "./components/navbar/Navbar";
 import SignIn from "./components/signup/SignIn";
 import Signup from "./components/signup/Signup";
 import Todo from "./components/todo/Todo";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { authActions } from "./store";
+
 
 const App = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const id = sessionStorage.getItem("id");
+    if (id) {
+      dispatch(authActions.login());
+    }
+  },
+    []);
+  
   return (
     <div>
       <Router>
         <Navbar />
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route  path="/about" element={<About />} />
-          <Route  path="/todo" element={<Todo />} />
-          <Route  path="/signup" element={<Signup />} />
-          <Route  path="/signin" element={<SignIn />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<SignIn />} />
         </Routes>
       </Router>
 
